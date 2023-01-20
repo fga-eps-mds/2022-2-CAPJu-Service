e6afc85fb302a11dc6a71e38af528dd1484fcf69
PedroHenrique2077 <pedrozinho.herique11@gmail.com>
2023-01-19 21:44:50 -0300

    adicionando estilos ao email
    Co-authored-by: LucasLopesFrazao <211031771@aluno.unb.br>

--- src/controllers/EmailController.js
+++ src/controllers/EmailController.js
@@ -1,4 +1,7 @@
 import * as nodemailer from "nodemailer";
+import handlebars from "handlebars";
+import fs from "fs";
+import path from "path";
 
 class EmailController {
   async sendEmail(req, res) {
@@ -15,87 +18,37 @@ class EmailController {
       },
     });
 
+    const __dirname = path.resolve();
+    const filePath = path.join(__dirname, "src/emails/template.html");
+    const source = fs.readFileSync(filePath, "utf-8").toString();
+    const template = handlebars.compile(source);
+
+    const replacements = {
+      username: "Vistoria médica",
+    };
+    const htmlToSend = template(replacements);
+
     const message = {
       from: "capju.eps.mds@gmail.com",
       to: "capju.eps.mds@gmail.com",
-      subject: "Processos atrasados",
+      subject: "Atrasos nos processos",
       text: "Olá, esse é um e-mail automático para informar os processos atrasados.",
-      html: `
-      <div class="table-wrapper">
-      <table class="fl-table">
-        <thead>
-          <tr>
-            <th>Fluxo</th>
-            <th>Processo</th>
-            <th>Etapa</th>
-            <th>Data de inicio</th>
-            <th>Duração (em dias)</th>
-            <th>Tempo atrasado (em dias)</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Fluxo de aposentadoria</td>
-            <td>132456</td>
-            <td>Peritagem médica</td>
-            <td>01/01/2023</td>
-            <td>10</td>
-            <td>6</td>
-          </tr>
-          <tr>
-            <td>Fluxo de aposentadoria</td>
-            <td>654321</td>
-            <td>Vistoria médica</td>
-            <td>10/01/2023</td>
-            <td>5</td>
-            <td>2</td>
-          </tr>
-          <tr>
-            <td>Fluxo de aposentadoria</td>
-            <td>753951</td>
-            <td>Aprovação do juiz</td>
-            <td>10/01/2023</td>
-            <td>5</td>
-            <td>2</td>
-          </tr>
-          <tr>
-            <td>Fluxo de invalidez</td>
-            <td>745896</td>
-            <td>Peritagem médica</td>
-            <td>10/12/2022</td>
-            <td>30</td>
-            <td>7</td>
-          </tr>
-          <tr>
-            <td>Fluxo de invalidez</td>
-            <td>123321</td>
-            <td>Peritagem médica</td>
-            <td>02/01/2023</td>
-            <td>8</td>
-            <td>7</td>
-          </tr>
-          <tr>
-            <td>Fluxo de divorcio</td>
-            <td>741852</td>
-            <td>Assinatura do advogado</td>
-            <td>28/12/2022</td>
-            <td>7</td>
-            <td>13</td>
-          </tr>
-          <tr>
-            <td>Fluxo trabalhista</td>
-            <td>963852</td>
-            <td>Pagamento</td>
-            <td>10/01/2023</td>
-            <td>3</td>
-            <td>4</td>
-        </tr>
-        </tbody>
-
-        <tbody></tbody>
-      </table>
-    </div>
-      `,
+      html: htmlToSend,
+      attachments: [{
+        filename: "capju.png",
+        path: __dirname + "/src/emails/capju.png",
+        cid: "capju",
+      },
+      {
+        filename: "justica_federal.png",
+        path: __dirname + "/src/emails/justica_federal.png",
+        cid: "justica_federal",
+      },
+      {
+        filename: "UnB.png",
+        path: __dirname + "/src/emails/UnB.png",
+        cid: "UnB",
+      },],
     };
 
     transport.sendMail(message, (err) => {
